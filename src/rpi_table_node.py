aa3c289aeb51547cbe40ce3297af7644b6edeeb0
GalSajko <gal.sajko@gmail.com>
2021-07-28 15:10:07 +0000

    Added stl filename as parameter.

--- src/rpi_table_node.py
+++ src/rpi_table_node.py
@@ -6,18 +6,20 @@ from robot_module_msgs.srv import StlFileManager
 
 def stlHandler(req):
     global thisTableName
+    global stlFilename
+
     success = False
-    stlFilename = ''
+    stl = ''
     if req.tableName == thisTableName:
-        stlFilename = thisTableName
+        stl = stlFilename
         success = True
     
-    return stlFilename, success
-
+    return stl, success
 
 if __name__ == '__main__':
     rospy.init_node('rpi_table_node', anonymous = True)
     thisTableName = rospy.get_param('~tableName')
+    stlFilename = rospy.get_param('~stlFile')
     stlFileService = rospy.Service('stl_file_service_' + thisTableName, StlFileManager, stlHandler)
 
     rospy.spin()
